644b428f4e9717df3b1690dc57e305d86ffcef75
Truong-Duong-Anh-Tu <truongtu11747@gmail.com>
2026-01-01 23:40:54 +0700

    complete feature Hr reward point, fix ConversionRule
    kết nối front back end chức năng tặng điểm cho nhân viên, sửa fetch thông tin COnversion rule point to money

--- src/frontend/src/pages/profile/ProfilePage.tsx
+++ src/frontend/src/pages/profile/ProfilePage.tsx
@@ -24,7 +24,7 @@ export default function ProfilePage() {
   setShowUpdateForm(false);
 };
 
-  console.log("Employee Data:", data);
+  // console.log("Employee Data:", data);
 
   // Loading state
   if (loading) {
